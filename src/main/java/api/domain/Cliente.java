89447ac30d0cfd6edd80e76c46687f0ee0a6bed2
jrfertonani <ademirjuniorfertonani@gmail.com>
2024-07-19 12:24:23 -0300

    ajuste de serealização  de classes de dominio

--- src/main/java/api/domain/Cliente.java
+++ src/main/java/api/domain/Cliente.java
@@ -1,5 +1,6 @@
 package api.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
@@ -16,6 +17,7 @@ import java.util.List;
 @Data      @Entity
 public class Cliente extends Pessoa{
 
+    @JsonIgnore   // para evitar a serealização, chamar todos os chamados de uma vez
     @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
--- src/main/java/api/domain/Tecnico.java
+++ src/main/java/api/domain/Tecnico.java
@@ -1,5 +1,6 @@
 package api.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
@@ -17,6 +18,7 @@ import java.util.List;
 @Entity
 public class Tecnico extends Pessoa {
 
+    @JsonIgnore   // para evitar a serealização, chamar todos os chamados deste tecnico de uma vez
     @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
