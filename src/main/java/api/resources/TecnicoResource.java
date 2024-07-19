b8fe80dd733a063121bbecfdd283cd7b606245f4
jrfertonani <ademirjuniorfertonani@gmail.com>
2024-07-19 12:21:10 -0300

    resource / tecnico

---
+++ src/main/java/api/resources/TecnicoResource.java
@@ -0,0 +1,24 @@
+package api.resources;
+
+import api.domain.Tecnico;
+import api.services.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tecnicos")
+public class TecnicoResource {
+
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(tecnicoService.findById(id));
+    }
+
+}
---
+++ src/main/java/api/services/TecnicoService.java
@@ -0,0 +1,22 @@
+package api.services;
+
+import api.domain.Tecnico;
+import api.repositories.TecnicoRepository;
+import api.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    public Tecnico findById(Integer id) {
+        Optional<Tecnico> obj = tecnicoRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
